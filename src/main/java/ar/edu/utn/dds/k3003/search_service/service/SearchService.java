a4258d5ad886f6a33e2eb1bb1e751994d4aca813
Matias Nuñez <116457068+YOMYMOY@users.noreply.github.com>
2025-11-15 13:01:04 -0300

    Add HashSet import to SearchService.java

--- src/main/java/ar/edu/utn/dds/k3003/search_service/service/SearchService.java
+++ src/main/java/ar/edu/utn/dds/k3003/search_service/service/SearchService.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashSet;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
